59cb2d6087f69cd25fe9cbec8143761fd230f596
veritasdol <veritas.dol@gmail.com>
2023-05-17 18:48:53 +0200

    adding objects

--- code/editor.py
+++ code/editor.py
@@ -37,6 +37,20 @@ class Editor:
 
 		self.menu = Menu()
 
+		# objects
+		self.canvas_objects = pygame.sprite.Group()
+		self.object_drag_active = False
+
+		# Player
+		CanvasObject(pos=(200,WINDOW_HEIGHT/2), 
+			frames=self.animations[0]['frames'], 
+			tile_id= 0,origin=self.origin, 
+			group=self.canvas_objects)
+
+		# sky
+		self.sky_handle = CanvasObject(pos=(WINDOW_WIDTH/2, WINDOW_HEIGHT/2), 
+			frames=[self.sky_handle_surf], tile_id=1, origin=self.origin, group=self.canvas_objects)
+
 	# support
 	def get_current_cell(self):
 		distance_to_origin = vector(mouse_pos()) - self.origin
@@ -79,7 +93,8 @@ class Editor:
 							self.canvas_data[cell].terrain_neighbors.append(name)
 
 	def imports(self):
-		self.water_bottom = load('../graphics/terrain/water/water_bottom.png')
+		self.water_bottom = load('../graphics/terrain/water/water_bottom.png').convert_alpha()
+		self.sky_handle_surf = load('../graphics/cursors/handle.png').convert_alpha()
 
 		# animations
 		self.animations = {}
@@ -107,6 +122,7 @@ class Editor:
 			self.pan_input(event)
 			self.selection_hotkeys(event)
 			self.menu_click(event)
+			self.object_drag(event)
 			self.canvas_add()
 			self.canvas_remove()
 	
@@ -115,18 +131,25 @@ class Editor:
 			self.selection_index = self.menu.click(mouse_pos(), mouse_buttons())
 
 	def canvas_add(self):
-		if mouse_buttons()[0] and not self.menu.rect.collidepoint(mouse_pos()):
+		if mouse_buttons()[0] and not self.menu.rect.collidepoint(mouse_pos()) and not self.object_drag_active:
 			current_cell = self.get_current_cell()
+			if EDITOR_DATA[self.selection_index]['type'] == 'tile':
 
-			if current_cell != self.last_selected_cell:
+				if current_cell != self.last_selected_cell:
 
-				if current_cell in self.canvas_data:
-					self.canvas_data[current_cell].add_id(self.selection_index)
-				else:
-					self.canvas_data[current_cell] = CanvasTile(self.selection_index)
+					if current_cell in self.canvas_data:
+						self.canvas_data[current_cell].add_id(self.selection_index)
+					else:
+						self.canvas_data[current_cell] = CanvasTile(self.selection_index)
 
-				self.check_neighbors(current_cell)
-				self.last_selected_cell = current_cell
+					self.check_neighbors(current_cell)
+					self.last_selected_cell = current_cell
+			else:
+				CanvasObject(pos=mouse_pos(), 
+				frames=self.animations[self.selection_index]['frames'],
+				tile_id=self.selection_index,
+				origin=self.origin,
+				group=self.canvas_objects)
 
 	def canvas_remove(self):
 		if mouse_buttons()[2] and not self.menu.rect.collidepoint(mouse_pos()):
@@ -153,13 +176,20 @@ class Editor:
 		if event.type == pygame.MOUSEWHEEL:
 			if pygame.key.get_pressed()[pygame.K_LCTRL]:
 				self.origin.y -= event.y * 50
+				for sprite in self.canvas_objects:
+					sprite.pan_pos(self.origin)
 			else:
 				self.origin.x -= event.y * 50
+				for sprite in self.canvas_objects:
+					sprite.pan_pos(self.origin)
 
 
 		# panning update
 		if self.pan_active:
 			self.origin = vector(mouse_pos()) - self.pan_offset
+
+			for sprite in self.canvas_objects:
+				sprite.pan_pos(self.origin)
 
 	def selection_hotkeys(self, event):
 		if event.type == pygame.KEYDOWN:
@@ -169,6 +199,17 @@ class Editor:
 				self.selection_index -= 1
 		self.selection_index = max(min(self.selection_index, 18), 2)
 		
+	def object_drag(self, event):
+		if event.type == pygame.MOUSEBUTTONDOWN and mouse_buttons()[0]:
+			for sprite in self.canvas_objects:
+				if sprite.rect.collidepoint(event.pos):
+					sprite.start_drag()
+					self.object_drag_active = True
+		if event.type == pygame.MOUSEBUTTONUP and self.object_drag_active:
+			for sprite in self.canvas_objects:
+				if sprite.selected:
+					sprite.drag_end(self.origin)
+					self.object_drag_active = False
 	# drawing
 	def draw_tile_lines(self):
 		cols = WINDOW_WIDTH // TILE_SIZE
@@ -228,6 +269,7 @@ class Editor:
 				surf = frames[index]
 				rect = surf.get_rect(midbottom = (pos[0] + TILE_SIZE // 2, pos[1] + TILE_SIZE))
 				self.display_surface.blit(surf, rect)
+		self.canvas_objects.draw(self.display_surface)
 
 	# update
 	def run(self, dt):
@@ -235,6 +277,7 @@ class Editor:
 
 		# updating
 		self.animation_update(dt)
+		self.canvas_objects.update(dt)
 
 		# drawings
 		self.display_surface.fill('gray')
@@ -289,3 +332,44 @@ class CanvasTile:
 			self.is_empty = True
 
 
+class CanvasObject(pygame.sprite.Sprite):
+	def __init__(self, pos, frames, tile_id, origin, group):
+		super().__init__(group)
+		self.tile_id = tile_id
+
+		# animation
+		self.frames = frames
+		self.frame_index = 0
+		
+		self.image = self.frames[self.frame_index]
+		self.rect = self.image.get_rect(center=pos)
+
+		# movement
+		self.distance_to_origin = vector(self.rect.topleft) - origin
+		self.selected = False
+		self.mouse_offset = vector(0, 0)
+
+	def start_drag(self):
+		self.selected = True
+		self.mouse_offset = vector(mouse_pos()) - vector(self.rect.topleft)
+
+	def drag(self):
+		if self.selected:
+			self.rect.topleft = mouse_pos() - self.mouse_offset
+
+	def drag_end(self, origin):
+		self.selected = False
+		self.distance_to_origin = vector(self.rect.topleft) - origin
+
+	def animate(self, dt):
+		self.frame_index += ANIMATION_SPEED * dt
+		self.frame_index = 0 if self.frame_index >= len(self.frames) else self.frame_index
+		self.image = self.frames[int(self.frame_index)]
+		self.rect = self.image.get_rect(midbottom = self.rect.midbottom)
+
+	def pan_pos(self, origin):
+		self.rect.topleft = origin + self.distance_to_origin
+
+	def update(self, dt):
+		self.animate(dt)
+		self.drag()
--- code/support.py
+++ code/support.py
@@ -7,7 +7,7 @@ def import_folder(path):
     for folder_name, sub_folders, img_files in walk(path):
         for img_name in img_files:
             full_path = path + '/' + img_name
-            image_surf = pygame.image.load(full_path)
+            image_surf = pygame.image.load(full_path).convert_alpha()
             surface_list.append(image_surf)
     return surface_list
 
@@ -17,6 +17,6 @@ def import_folder_dict(path):
     for folder_name, sub_folders, img_files in walk(path):
         for img_name in img_files:
             full_path = path + '/' + img_name
-            image_surf = pygame.image.load(full_path)
+            image_surf = pygame.image.load(full_path).convert_alpha()
             surface_dict[img_name.split('.')[0]] = image_surf
     return surface_dict
