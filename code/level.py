eada4b2f257a0f12e1b2077d45b5f64353dd8fb2
veritasdol <veritas.dol@gmail.com>
2023-05-28 18:47:48 +0200

    terrain and player in the level

--- code/level.py
+++ code/level.py
@@ -3,11 +3,28 @@ from pygame.math import Vector2 as vector
 
 from support import *
 from settings import *
+from sprites import Generic, Player
 
 class Level:
-    def __init__(self, grid, switch) -> None:
+    def __init__(self, grid, switch, asset_dict) -> None:
         self.display_surface = pygame.display.get_surface()
         self.switch = switch
+
+        # groups
+        self.all_sprites = pygame.sprite.Group()
+
+        self.build_level(grid, asset_dict)
+
+    def build_level(self, grid, asset_dict):
+        for layer_name, layer in grid.items():
+            for pos, data in layer.items():
+                if layer_name == 'terrain':
+                    Generic(pos, asset_dict['land'][data], self.all_sprites)
+                
+                match data:
+                    case 0: self.player = Player(pos, self.all_sprites)
+
+
 
     def event_loop(self):
         for event in pygame.event.get():
@@ -19,4 +36,6 @@ class Level:
 
     def run(self, dt):
         self.event_loop()
-        self.display_surface.fill('red')
+        self.display_surface.fill(SKY_COLOR)
+        self.all_sprites.update(dt)
+        self.all_sprites.draw(self.display_surface)
--- code/main.py
+++ code/main.py
@@ -33,7 +33,9 @@ class Main:
     def switch(self, grid = None):
         self.transition.active = True
         if grid:
-            self.level = Level(grid, self.switch)
+            self.level = Level(grid, self.switch, {
+                'land': self.land_tiles
+            })
 
     def run(self):
         while True:
